5d133f6c0eb17301358b0759401c97d5857cd204
gurpreetsingh00885 <gurpreetsingh00885@gmail.com>
2017-02-15 03:46:51 +0530

    Add display. Use z to toggle fullscreen. ctrl + alt + shift + x to clear all stats.

--- board1.py
+++ board1.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 size = (1280, 800)			# (1280, 600)screen resolution
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size, FULLSCREEN)
 
 def readData(slot):				# slot is the time slot ;)
 	dataFile = open(["team_data1.dat", "team_data2.dat",][slot-1], "r")
@@ -24,7 +24,7 @@ def dumpData(slot,data):
 
 def main():
 	slot = 1 					# slot number
-
+	fullscreen = True
 	while True:
 		screen.fill((255,255,255))	# background color
 		data = readData(slot)
@@ -32,20 +32,46 @@ def main():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
+			if event.type is KEYDOWN and event.key == K_x:
+				if (pygame.key.get_mods() & pygame.KMOD_ALT) and (pygame.key.get_mods() & pygame.KMOD_CTRL) and (pygame.key.get_mods() & pygame.KMOD_SHIFT):
+					for team in data:
+						data[team][1]=data[team][2]=0
+						dumpData(slot, data)
 			if event.type is KEYDOWN and event.key in [ord(x) for x in data.keys()]:
 				if pygame.key.get_mods() & pygame.KMOD_ALT:
 					data[chr(event.key)][2] += 1 			# Add 1 loss
 				if pygame.key.get_mods() & pygame.KMOD_CTRL:
 					data[chr(event.key)][1] += 1 			# Add 1 win
-				print data[chr(event.key)]
-			
-			if event.type is KEYDOWN and event.key == K_w:
-				pygame.display.set_mode(size)
 			if event.type is KEYDOWN and event.key == K_z:
-				pygame.display.set_mode(size, FULLSCREEN)
+				if fullscreen:
+					pygame.display.set_mode(size)
+					fullscreen = False
+				else:
+					pygame.display.set_mode(size, FULLSCREEN)
+					fullscreen = True
 			if event.type is KEYDOWN and event.key == K_TAB:
 				slot = 1 if slot == 2 else 2
+				data = readData(slot)
 		displayText("Slot %s" %slot, 20, (0,0,0), (1240,0))
+		byWins = sorted(sorted(data.values()), key=lambda x:x[1], reverse=True)		# Ranking teams
+		#displayText("P.U.L.o.G.", 40, (255,100,0),(560,10))
+		displayText("Counter Strike 1.6 Tournament", 60, (0,0,255), (320, 20))
+		displayText("Scoreboard", 50, (255,100,0),(540,70))
+		displayText("Rank", 45, (0,0,0), (130,120))
+		displayText("Team Name", 45, (0,0,0), (280,120))
+		displayText("Wins", 45, (0,0,0), (680,120))
+		displayText("Losses", 45, (0,0,0), (830,120))
+		displayText("Win %age", 45, (0,0,0), (980,120))
+		startY = 180 
+		for i in range(len(byWins)):
+			displayText("%d" %(i+1), 45, (0,100,0), (160,startY))
+			displayText("%s" %(byWins[i][0]), 40, (0,100,100), (300,startY))
+			displayText("%d" %(byWins[i][1]), 45, (0,255,0), (700,startY))
+			displayText("%d" %(byWins[i][2]), 45, (255,0,0), (860,startY))
+			matches = byWins[i][2]+byWins[i][1]
+			displayText("%d%%" %((float(byWins[i][1])/(byWins[i][2]+byWins[i][1]))*100 if matches>0 else 0), 45, (0,0,255), (1010,startY))
+			
+			startY += 80
 		dumpData(slot, data)
 		pygame.display.update()
 
