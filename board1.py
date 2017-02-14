dd59fe3b7f6c8d43bd2fe5cda7a18b24bc194be3
gurpreetsingh00885 <gurpreetsingh00885@gmail.com>
2017-02-15 02:25:51 +0530

    Use binary files & implement scoring mech.

--- board1.py
+++ board1.py
@@ -1,21 +1,30 @@
-import pygame, sys
+import pygame, sys, pickle
 from pygame.locals import *
 
 pygame.init()
+size = (1280, 800)			# (1280, 600)screen resolution
+screen = pygame.display.set_mode(size)
 
 def readData(slot):				# slot is the time slot ;)
-	dataFile = open(["team_data1.txt", "team_data2.txt",][slot-1], "r")
-	data = []
-	for team in dataFile.readlines():
-		temp = team.split(',')
-		data.append([temp[0], int(temp[1]), int(temp[1]), temp[-1][:-1]])
+	dataFile = open(["team_data1.dat", "team_data2.dat",][slot-1], "r")
+	data = pickle.load(dataFile)
 	dataFile.close()
 	return data
 
+def displayText(text, size, color, position):
+	myfont = pygame.font.SysFont("monospace", size)	# render text
+	label = myfont.render(text, 1, color)
+	screen.blit(label, position)
+
+def dumpData(slot,data):
+	dataFile = open(["team_data1.dat", "team_data2.dat",][slot-1], "w")
+	pickle.dump(data, dataFile)
+	dataFile.close()
+
+
 def main():
-	size = (1280, 800)			# (1280, 600)screen resolution
 	slot = 1 					# slot number
-	screen = pygame.display.set_mode(size)
+
 	while True:
 		screen.fill((255,255,255))	# background color
 		data = readData(slot)
@@ -23,17 +32,21 @@ def main():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
+			if event.type is KEYDOWN and event.key in [ord(x) for x in data.keys()]:
+				if pygame.key.get_mods() & pygame.KMOD_ALT:
+					data[chr(event.key)][2] += 1 			# Add 1 loss
+				if pygame.key.get_mods() & pygame.KMOD_CTRL:
+					data[chr(event.key)][1] += 1 			# Add 1 win
+				print data[chr(event.key)]
+			
 			if event.type is KEYDOWN and event.key == K_w:
 				pygame.display.set_mode(size)
-			if event.type is KEYDOWN and event.key == K_f:
+			if event.type is KEYDOWN and event.key == K_z:
 				pygame.display.set_mode(size, FULLSCREEN)
 			if event.type is KEYDOWN and event.key == K_TAB:
 				slot = 1 if slot == 2 else 2
-				print data
-		myfont = pygame.font.SysFont("monospace", 70)
-
-		# render text
-		label = myfont.render("Some text!", 1, (255,255,0))
-		screen.blit(label, (100, 100))
+		displayText("Slot %s" %slot, 20, (0,0,0), (1240,0))
+		dumpData(slot, data)
 		pygame.display.update()
+
 main()
