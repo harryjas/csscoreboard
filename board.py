1e51e4899e8451fb6243823c95d2ef666d617d36
gurpreetsingh00885 <gurpreetsingh00885@gmail.com>
2017-02-16 19:54:37 +0530

    final changes.

--- board.py
+++ board.py
@@ -11,8 +11,12 @@ def readData(slot):				# slot is the time slot ;)
 	dataFile.close()
 	return data
 
-def displayText(text, size, color, position):
-	myfont = pygame.font.SysFont("monospace", size)	# render text
+def displayText(text, size, color, position, myfont = ""):
+	#myfont = pygame.font.Font("neuropolxrg.ttf", size-15)
+	if myfont == "":
+		myfont = pygame.font.SysFont("monospace", size)
+	else:
+		myfont = pygame.font.Font(myfont, size-15)
 	label = myfont.render(text, 1, color)
 	screen.blit(label, position)
 
@@ -26,7 +30,13 @@ def main():
 	slot = 1 					# slot number
 	fullscreen = True
 	logs = []
+	col = 0
+	change = 10
 	while True:
+		col += change
+		if col >= 240 or col<=0:
+			change *= -1
+
 		screen.fill((255,255,255))	# background color
 		data = readData(slot)
 		for event in pygame.event.get():
@@ -64,21 +74,27 @@ def main():
 		displayText("Slot %s" %slot, 20, (0,0,0), (1240,0))
 		byWins = sorted(sorted(data.values()), key=lambda x:x[1], reverse=True)		# Ranking teams
 		#displayText("P.U.L.o.G.", 40, (255,100,0),(560,10))
-		displayText("Counter Strike 1.6 Tournament", 60, (0,0,255), (320, 20))
-		displayText("Scoreboard", 50, (255,100,0),(540,70))
-		displayText("Rank", 45, (0,0,0), (130,120))
-		displayText("Team Name", 45, (0,0,0), (280,120))
-		displayText("Wins", 45, (0,0,0), (680,120))
-		displayText("Losses", 45, (0,0,0), (830,120))
-		displayText("Win %age", 45, (0,0,0), (980,120))
+		displayText("Counter Strike 1.6 Tournament", 60, (col,0,255-col), (250, 20),"neuropolxrg.ttf")
+		displayText("Scoreboard", 50, (col,100,0),(500,70),"neuropolxrg.ttf")
+		displayText("Rank", 45, (0,0,0), (100,120))
+		displayText("Team Name", 45, (0,0,0), (250,120))
+		displayText("Code", 45, (0,0,0), (580,120))
+		displayText("Wins", 45, (0,0,0), (710,120))
+		displayText("Losses", 45, (0,0,0), (860,120))
+		displayText("Win %age", 45, (0,0,0), (1010,120))
 		startY = 180 
 		for i in range(len(byWins)):
-			displayText("%d" %(i+1), 45, (0,100,0), (160,startY))
-			displayText("%s" %(byWins[i][0]), 40, (0,100,100), (300,startY))
-			displayText("%d" %(byWins[i][1]), 45, (0,255,0), (710,startY))
-			displayText("%d" %(byWins[i][2]), 45, (255,0,0), (870,startY))
+			code = ''
+			for key, value in data.items():
+				if value[0] == byWins[i][0]:
+					code = key
+			displayText("%d" %(i+1), 50, (0,100,0), (120,startY), "digital-7.ttf")
+			displayText("%s" %(byWins[i][0]), 40, (0,100,100), (280,startY))
+			displayText("#%s" %(code), 35, (0,0,0), (600,startY))
+			displayText("%d" %(byWins[i][1]), 50, (0,255,0), (740,startY), "digital-7.ttf")
+			displayText("%d" %(byWins[i][2]), 50, (255,0,0), (900,startY), "digital-7.ttf")
 			matches = byWins[i][2]+byWins[i][1]
-			displayText("%d%%" %((float(byWins[i][1])/(byWins[i][2]+byWins[i][1]))*100 if matches>0 else 0), 45, (0,0,255), (1020,startY))
+			displayText("%d%%" %((float(byWins[i][1])/(byWins[i][2]+byWins[i][1]))*100 if matches>0 else 0), 50, (0,0,255), (1050,startY), "digital-7.ttf")
 			startY += 80
 
 		for log in logs[::-1]:
@@ -88,8 +104,8 @@ def main():
 			displayText("Recent", 40, (0,0,0), (20,680))
 			displayText("Events", 40, (0,0,0), (20,710))
 		displayText("Powered By ", 30, (0,0,0), (750,700))
-		displayText("Kill 'em ", 50, (0,0,0), (1110,640))
-		displayText("All!", 60, (0,0,0), (1130,680))
+		displayText("Kill 'em ", 50, (0,col,0), (1110,640), "neuropolxrg.ttf")
+		displayText("All!", 60, (0,0,col), (1130,680), "neuropolxrg.ttf")
 		screen.blit(logo, (900,630))
 		dumpData(slot, data)
 		pygame.display.update()
